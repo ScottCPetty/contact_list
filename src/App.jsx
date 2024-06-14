a4fc68e2dd986914d05c97246b5d451d759d85f2
ScottCPetty <scottishcheese13@gmail.com>
2024-06-14 12:30:37 -0400

    Entered dummyContacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import ContactList from "./components/ContactList";
 
 function App() {
 
   return (
     <>
+      <ContactList />
     </>
   );
 }
