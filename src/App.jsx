95adc97fc320a426dd4e1785ff46957972a0870f
ScottCPetty <scottishcheese13@gmail.com>
2024-06-14 13:08:53 -0400

    Error when selecting contact view

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import "./App.css";
 import ContactList from "./components/ContactList";
 
 function App() {
-
+  const [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
